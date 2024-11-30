6394cdb8bc8d27039353a5bcc7adaf95a2300ad9
el-dada acile <acile.el-dada@etu.univ-cotedazur.fr>
2024-11-30 22:43:10 +0100

    Amelioration page accueil + ajout cartes cookies (a finir)

--- View/common/footer.php
+++ View/common/footer.php
@@ -3,10 +3,5 @@
 
 
 
-
-
-
-
-
 <!-- correspondra que à la partie footer, qui sera donc réutilisée sur
 toutes les pages de l'interface -->
--- View/common/header.php
+++ View/common/header.php
@@ -9,37 +9,44 @@
     <link rel="stylesheet" href="/PHP/css/vuPublique.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
-
+    <link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet">
 
 </head>
 
 <body>
-    <header>
-            <nav>
-                <div class="nav-left">
-                    <a href="index.php" class="logo-link">
-                        <img src="/PHP/assets/logo.png" alt="M&A Cookies"
-                        class="logo-img">
-                    </a>
-                </div>
+<header>
+    <nav>
+        <div class="nav-left">
+            <a href="index.php" class="logo-link">
+                <img src="/PHP/assets/logo.png" alt="M&A Cookies" class="logo-img">
+            </a>
+        </div>
 
-                <div class="menu">
-                    <a href="">Nos cookies</a>
-                    <a href="">Contact</a>
-                    <a href="" class="cart-wrapper">
-                        <div class="cart-icon">
-                            <img src="/PHP/assets/cart.png" alt="cart"
-                            style="width: 28px; height: 28px;">
-                            <div class="notification">0</div> <!-- pour l'instant 0 mais à voir comment faire pour incrémenter les numero d'articles -->
+        <div class="menu">
+            <a href="">Nos cookies</a>
+            <a href="">Contact</a>
+
+            
+            <label for="search-toggle" class="search">
+                <div class="search-icon">
+                    <img src="/PHP/assets/search.png" alt="search" style="width: 28px; height: 28px;">
+                </div>
+            </label>
+
+            
+            <input type="checkbox" id="search-toggle" class="search-toggle">
+
+            
+            <div class="search-field">
+                <input type="text" placeholder="Rechercher..." id="search-input">
+            </div>
+
+            <a href="" class="cart-wrapper">
+                <div class="cart-icon">
+                    <img src="/PHP/assets/cart.png" alt="cart" style="width: 28px; height: 28px;">
+                    <div class="notification">0</div> <!-- pour l'instant 0 mais à voir comment faire pour incrémenter les numéros d'articles -->
                 </div>
             </a>
         </div>
     </nav>
 </header>
-
-
-<!-- correspondra que à la partie header, qui sera donc réutilisée sur
-toutes les pages de l'interface -->
-
-
